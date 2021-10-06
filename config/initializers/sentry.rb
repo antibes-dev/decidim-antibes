362757d561ed75562a0d4cf69c3a67567bde3dd2
Forget Sébastien <sebastien.forget@ville-antibes.fr>
2021-10-06 16:39:43 +0200

    Update sentry.rb

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -7,8 +7,6 @@ if Rails.application.secrets.dig(:sentry, :enabled)
     config.dsn = Rails.application.secrets.dig(:sentry, :dsn)
     config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-    # To activate performance monitoring, set one of these options.
-    # We recommend adjusting the value in production:
     config.traces_sample_rate = ENV.fetch("SENTRY_SAMPLE_RATE", 0.5)
   end
 end
