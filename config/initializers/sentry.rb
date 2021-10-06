2e29221771487eac6a5d202b41072b6a14415990
Forget Sébastien <sebastien.forget@ville-antibes.fr>
2021-10-06 11:16:53 +0200

    Update sentry.rb

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,7 +4,6 @@ require "sentry-ruby"
 
 if Rails.application.secrets.dig(:sentry, :enabled)
   Sentry.init do |config|
-    # config.dsn = 'http://3911b1ade4414a7db38719db21c35a7f@sf-sentry-dev.ville-antibes.fr:9000/2'
     config.dsn = Rails.application.secrets.dig(:sentry, :dsn)
     config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
